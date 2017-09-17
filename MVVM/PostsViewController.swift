9b841d00c74cb2fd5c6152db3ddf5658b9821153
Mattia <m.contin.dev@gmail.com>
2017-09-17 12:46:50 +0200

    Setup "didSelect" callback for posts table view

--- MVVM/PostsViewController.swift
+++ MVVM/PostsViewController.swift
@@ -21,12 +21,19 @@ class PostsViewController: UIViewController {
         super.viewDidLoad()
         
         postsViewModel.posts.asObservable()
-            .bind(to: tableView.rx.items(cellIdentifier: "PostCell", cellType: PostCell.self)) { (row, element, cell) in
+            .bind(to: tableView.rx.items(cellIdentifier: PostCell.kCellIdentifier, cellType: PostCell.self))
+            { (row, element, cell) in
                 cell.authorLabel.text = element.author?.username
                 cell.titleLabel.text = element.title
                 cell.previewLabel.text = element.body
             }
             .disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected
+            .subscribe { indexPath in
+                // go to post detail controller
+            }
+            .addDisposableTo(disposeBag)
         
         postsViewModel.posts.value = PostsStore.getMockPosts()
     }
